e3df2fe8f24d28ad309719dd3aa4503ecff232c2
Stepan Chekhovskoi <chestedos@gmail.com>
2026-06-05 20:17:52 +0200

    taker whitelist opcode

---
+++ src/instructions/Whitelist.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: LicenseRef-Degensoft-SwapVM-1.1
+pragma solidity 0.8.30;
+
+/// @custom:license-url https://github.com/1inch/swap-vm/blob/main/LICENSES/SwapVM-1.1.txt
+/// @custom:copyright © 2026 Degensoft Ltd
+
+import { Calldata } from "@1inch/solidity-utils/contracts/libraries/Calldata.sol";
+import { Context, ContextLib } from "../libs/VM.sol";
+
+library WhitelistArgsBuilder {
+    /// @dev For single taker it is efficient to pack full address
+    function buildWhitelistSingleTaker(address allowedTaker) internal pure returns (bytes memory) {
+        return abi.encodePacked(allowedTaker);
+    }
+
+    function parseWhitelistSingleTaker(bytes calldata args) internal pure returns (address allowedTaker) {
+        assembly ("memory-safe") {
+            allowedTaker := shr(96, calldataload(args.offset))
+        }
+    }
+
+    /// @dev Due to args length limitiation, for multiple takers only last 80 bits of each address is packed
+    function buildWhitelistMultipleTakers(address[] memory allowedTakers) internal pure returns (bytes memory) {
+        uint256 len = allowedTakers.length * 10;
+        bytes memory res = new bytes(len);
+
+        assembly ("memory-safe") {
+            let ptr := add(allowedTakers, add(32, 22))
+            let dst := add(res, 32)
+            let fin := add(dst, len)
+            for { } lt(dst, fin) { } {
+                mcopy(dst, ptr, 10)
+                ptr := add(ptr, 32)
+                dst := add(dst, 10)
+            }
+        }
+
+        return res;
+    }
+
+    function parseWhitelistMultipleTakers(bytes calldata args, uint256 id) internal pure returns (uint80 allowedTaker) {
+        assembly ("memory-safe") {
+            allowedTaker := shr(176, calldataload(add(args.offset, mul(id, 10))))
+        }
+    }
+
+    function wrapToPackedAddress(address taker) internal pure returns (uint80 packed) {
+        packed = uint80(uint160(taker));
+    }
+}
+
+/// @notice Set of functions for Taker validation
+contract Whitelist {
+    using WhitelistArgsBuilder for bytes;
+
+    error WhitelistInvalidTaker();
+
+    /// @notice Allows order to be executed only by the specified Taker
+    /// @param args.allowedTaker | 20 bytes
+    function _whitelistSingleTaker(Context memory ctx, bytes calldata args) internal pure {
+        address allowedTaker = args.parseWhitelistSingleTaker();
+        require(ctx.query.taker == allowedTaker, WhitelistInvalidTaker());
+    }
+
+    /// @notice Allows order to be executed only by one of specified Takers
+    /// @param args.allowedTakers[N] | 10 * N bytes last 10 bytes of each address is used
+    function _whitelistMultipleTakers(Context memory ctx, bytes calldata args) internal pure {
+        uint80 sender = WhitelistArgsBuilder.wrapToPackedAddress(ctx.query.taker);
+
+        unchecked {
+            uint256 i = args.length / 10;
+            while (i-- > 0) {
+                if (sender == args.parseWhitelistMultipleTakers(i)) return;
+            }
+        }
+
+        revert WhitelistInvalidTaker();
+    }
+}
