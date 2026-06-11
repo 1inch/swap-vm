6dc5e388b38d9d67f086032051af90713883fa43
Stepan Chekhovskoi <chestedos@gmail.com>
2026-06-11 17:14:01 +0200

    limit _whitelistSingleTaker args to 10 bytes

--- src/instructions/Whitelist.sol
+++ src/instructions/Whitelist.sol
@@ -11,12 +11,13 @@ library WhitelistArgsBuilder {
     error WhitelistEmptyList();
 
     function buildWhitelistSingleTaker(address allowedTaker) internal pure returns (bytes memory) {
-        return abi.encodePacked(allowedTaker);
+        return abi.encodePacked(uint80(uint160(allowedTaker)));
     }
 
-    function parseWhitelistSingleTaker(bytes calldata args) internal pure returns (address allowedTaker) {
+    /// @dev Due to calldata cost concern, only last 80 bits of address are packed
+    function parseWhitelistSingleTaker(bytes calldata args) internal pure returns (uint80 allowedTaker) {
         assembly ("memory-safe") {
-            allowedTaker := shr(96, calldataload(args.offset))
+            allowedTaker := shr(176, calldataload(args.offset))
         }
     }
 
@@ -55,20 +56,22 @@ library WhitelistArgsBuilder {
 /// @notice Set of functions for Taker validation
 /// @dev Taker whitelist functionality, the opcodes can be included in whatever place of the program
 /// @dev Partial account validation trade-off:
-/// - For packing multiple takers whitelist, only last 80 bits of each address are used
+/// - For packing taker addresses, only last 80 bits of each address are used
 /// - Mining 80 bits of Ethereum address is not truly impossible but would take years of millions GPUs time
 /// - Consider theoretical possibility of such address being mined for an address known for years,
-///   avoid orders with "free money" relying on the `_whitelistMultipleTakers` opcode
+///   avoid orders with "free money" relying on the opcodes
 contract Whitelist {
     using WhitelistArgsBuilder for bytes;
 
     error WhitelistInvalidTaker();
 
     /// @notice Allows order to be executed only by the specified Taker
-    /// @param args.allowedTaker | 20 bytes
+    /// @param args.allowedTaker | 10 bytes, last 10 bytes of address are used
     function _whitelistSingleTaker(Context memory ctx, bytes calldata args) internal pure {
-        address allowedTaker = args.parseWhitelistSingleTaker();
-        require(ctx.query.taker == allowedTaker, WhitelistInvalidTaker());
+        uint80 sender = WhitelistArgsBuilder.wrapToPackedAddress(ctx.query.taker);
+        uint80 allowedTaker = args.parseWhitelistSingleTaker();
+
+        require(sender == allowedTaker, WhitelistInvalidTaker());
     }
 
     /// @notice Allows order to be executed only by one of specified Takers
