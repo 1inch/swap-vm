257b9fc7098b30c89bc0adc7e500100fb1bce5d6
Dmitrii Kuzmin <dmitrykuzmin00@gmail.com>
2026-09-17 11:17:15 +0400

    chore: rename volume -> virtual balance in Decay.sol

--- contracts/instructions/Decay.sol
+++ contracts/instructions/Decay.sol
@@ -13,8 +13,8 @@ import { StorageSlots } from "../libs/StorageSlots.sol";
 import { InstructionBuilder } from "../libs/InstructionBuilder.sol";
 import { InstructionArgs } from "../libs/InstructionArgs.sol";
 
-/// @notice Decay opcode, increase balance in and decrease balance out by offsets decaying over time since last trade
-///   Offsets are increased at each swap by amount in and amount out against the current swap direction,
+/// @notice Decay opcode, increase balance in and decrease balance out by virtual balances decaying over time since last trade
+///   Virtual balances are increased at each swap by amount in and amount out against the current swap direction,
 ///   making immediate counter-swap have a worse price
 /// @dev Encoding: [uint16 period]
 /// @dev The opcode is expected to be executed only once in strategy flow, storage vars are written by the first-met opcode instance
@@ -44,23 +44,23 @@ library Decay {
         period = args.at(0).asU16();
     }
 
-    /// @dev Swap volumes for a single token.
-    struct TokenVolumes {
-        /// @dev Volume of token when it was swapped as token in.
-        DecayVolume asTokenIn;
-        /// @dev Volume of token when it was swapped as token out.
-        DecayVolume asTokenOut;
+    /// @dev Virtual balances for a single token.
+    struct TokenVirtualBalances {
+        /// @dev Virtual balance when this token was swapped as token in.
+        VirtualBalance asTokenIn;
+        /// @dev Virtual balance when this token was swapped as token out.
+        VirtualBalance asTokenOut;
     }
 
-    struct OrderTokenVolumes {
-        /// @dev Volumes for order token A.
-        TokenVolumes tokenA;
-        /// @dev Volumes for order token B.
-        TokenVolumes tokenB;
+    struct OrderTokensVirtualBalances {
+        /// @dev Virtual balances for order's token A.
+        TokenVirtualBalances tokenA;
+        /// @dev Virtual balances for order's token B.
+        TokenVirtualBalances tokenB;
     }
 
     struct Storage {
-        mapping(bytes32 orderHash => OrderTokenVolumes) orderTokenVolumes;
+        mapping(bytes32 orderHash => OrderTokensVirtualBalances) orderVirtualBalances;
     }
 
     function store() internal pure returns (Storage storage $) {
@@ -72,53 +72,54 @@ library Decay {
         Storage storage $ = store();
         uint16 period = parse(args);
 
-        OrderTokenVolumes storage volumes = $.orderTokenVolumes[ctx.query.orderHash];
+        OrderTokensVirtualBalances storage virtualBalances = $.orderVirtualBalances[ctx.query.orderHash];
 
-        TokenVolumes storage volumeTokenIn;
-        TokenVolumes storage volumeTokenOut;
-        if (ctx.query.tokenIn < ctx.query.tokenOut) (volumeTokenIn, volumeTokenOut) = (volumes.tokenA, volumes.tokenB);
-        else (volumeTokenIn, volumeTokenOut) = (volumes.tokenB, volumes.tokenA);
+        TokenVirtualBalances storage virtualBalanceTokenIn;
+        TokenVirtualBalances storage virtualBalanceTokenOut;
+        if (ctx.query.tokenIn < ctx.query.tokenOut) (virtualBalanceTokenIn, virtualBalanceTokenOut) = (virtualBalances.tokenA, virtualBalances.tokenB);
+        else (virtualBalanceTokenIn, virtualBalanceTokenOut) = (virtualBalances.tokenB, virtualBalances.tokenA);
 
-        ctx.swap.balanceIn += remainingVolume(volumeTokenIn.asTokenOut, period);
-        ctx.swap.balanceOut -= remainingVolume(volumeTokenOut.asTokenIn, period);
+        ctx.swap.balanceIn += remainingVirtualBalance(virtualBalanceTokenIn.asTokenOut, period);
+        ctx.swap.balanceOut -= remainingVirtualBalance(virtualBalanceTokenOut.asTokenIn, period);
 
-        uint216 volumeIn = remainingVolume(volumeTokenIn.asTokenIn, period);
-        uint216 volumeOut = remainingVolume(volumeTokenOut.asTokenOut, period);
+        uint216 virtualBalanceIn = remainingVirtualBalance(virtualBalanceTokenIn.asTokenIn, period);
+        uint216 virtualBalanceOut = remainingVirtualBalance(virtualBalanceTokenOut.asTokenOut, period);
 
         (uint256 amountIn, uint256 amountOut) = ctx.runLoop();
 
-        volumeIn += amountIn.toUint216();
-        volumeOut += amountOut.toUint216();
+        virtualBalanceIn += amountIn.toUint216();
+        virtualBalanceOut += amountOut.toUint216();
 
         if (!ctx.vm.isStaticContext) {
-            volumeTokenIn.asTokenIn = DecayVolumeLib.encode(volumeIn, uint40(block.timestamp));
-            volumeTokenOut.asTokenOut = DecayVolumeLib.encode(volumeOut, uint40(block.timestamp));
+            virtualBalanceTokenIn.asTokenIn = VirtualBalanceLib.encode(virtualBalanceIn, uint40(block.timestamp));
+            virtualBalanceTokenOut.asTokenOut = VirtualBalanceLib.encode(virtualBalanceOut, uint40(block.timestamp));
         }
     }
 
-    /// @dev Decay volume decreases linearly over time. Returns the remaining volume after given period.
-    function remainingVolume(DecayVolume data, uint16 period) internal view returns (uint216) {
+    /// @dev Virtual balance decreases linearly over time. Returns the remaining amount within the period.
+    function remainingVirtualBalance(VirtualBalance data, uint16 period) internal view returns (uint216) {
         unchecked {
-            (uint216 volume, uint40 ts) = DecayVolumeLib.decode(data);
+            (uint216 amount, uint40 ts) = VirtualBalanceLib.decode(data);
 
             uint256 expiration = uint256(ts) + period;
             if (block.timestamp >= expiration) return 0;
             uint256 timeLeft = expiration - block.timestamp;
 
             // timeLeft < period
-            return uint216(volume * timeLeft / period);
+            return uint216(amount * timeLeft / period);
         }
     }
 }
 
-type DecayVolume is uint256;
+/// @dev Virtual balance means the remaining amount of token that was swapped in previous swaps and not yet expired.
+type VirtualBalance is uint256;
 
-library DecayVolumeLib {
-    function encode(uint216 volume, uint40 ts) internal pure returns (DecayVolume) {
-        return DecayVolume.wrap((uint256(volume) << 40) | ts);
+library VirtualBalanceLib {
+    function encode(uint216 amount, uint40 ts) internal pure returns (VirtualBalance) {
+        return VirtualBalance.wrap((uint256(amount) << 40) | ts);
     }
 
-    function decode(DecayVolume data) internal pure returns (uint216 volume, uint40 ts) {
-        return (uint216(DecayVolume.unwrap(data) >> 40), uint40(DecayVolume.unwrap(data)));
+    function decode(VirtualBalance data) internal pure returns (uint216 amount, uint40 ts) {
+        return (uint216(VirtualBalance.unwrap(data) >> 40), uint40(VirtualBalance.unwrap(data)));
     }
 }
